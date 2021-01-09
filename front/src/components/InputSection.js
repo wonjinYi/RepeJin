0292cc76b689bb20bf69aa7525590ab29adadc05
wonjinYi <studioplug17@gmail.com>
2021-01-10 03:44:59 +0900

    create new components.

---
+++ front/src/components/InputSection.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function InputSection() {
+    return (
+        <InputSectionWrap>
+            INPUT
+        </InputSectionWrap>
+    );
+}
+
+const InputSectionWrap = styled.div`
+
+`;
---
+++ front/src/components/OutputSection.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function OutputSection() {
+    return (
+        <OutputSectionWrap>
+            OUTPUT
+        </OutputSectionWrap>
+    );
+}
+
+const OutputSectionWrap = styled.div`
+
+`;
