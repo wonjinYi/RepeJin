dde2098b806a60eeab863138cf599ef78604148f
wonjinYi <studioplug17@gmail.com>
2021-01-10 03:41:04 +0900

    beginning layout is set.

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,47 @@
-import react from "react";
+import styled from "styled-components";
+import { InputSection, OutputSection } from "./components";
 
-function App() {
-  return (
-    <AppWarp className="App">
-      
-    </AppWarp>
-  );
+export default function App() {
+    return (
+        <AppWrap className="App">
+            <HeaderWrap>
+                <Title>RepeJin</Title>
+            </HeaderWrap>
+
+            <ContentWrap>
+                <InputSection />
+                <OutputSection />
+            </ContentWrap>
+
+            <FooterWrap>
+                Wonjin Yi
+            </FooterWrap>
+        </AppWrap>
+    );
 }
 
-export default App;
+const AppWrap = styled.div`
+    display : flex;
+    flex-direction : column;
+    justify-content : center;
+    align-items : center;
+
+    color : white;
+`;
+
+const HeaderWrap = styled.div`
+    display : inherit;
+`;
+
+const ContentWrap = styled.div`
+    display : inherit;
+
+    border : 1px solid white;
+`;
+
+const FooterWrap = styled.div`
+`;
+
+const Title = styled.h1`
+    font
+`;
