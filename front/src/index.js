9e57030254b9f580f51a9d71366ef57547abcd15
wonjinYi <studioplug17@gmail.com>
2021-01-10 03:36:49 +0900

    add a element is linked to wonjinlab main

--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <a href="https://lab.wonj.in/" className="wonjinlab-link">Go to wonjinLab</a>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
