e5f1ec1117ab7787121f76e91ae9cd2c1fa3239c
wonjinYi <studioplug17@gmail.com>
2021-01-11 23:57:02 +0900

    add textfield, button

--- front/src/components/InputSection.js
+++ front/src/components/InputSection.js
@@ -1,13 +1,18 @@
 import styled from "styled-components";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default function InputSection() {
     return (
         <InputSectionWrap>
-            INPUT
+            <TextField id="standard-basic" label="Text" />
+            <TextField id="standard-basic" label="Reps" />
+            <Button variant="contained">DO REPEAT</Button>
         </InputSectionWrap>
     );
 }
 
 const InputSectionWrap = styled.div`
-
+    display : flex;
+    flex-direction : column;
 `;
