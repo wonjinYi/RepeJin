6e2e021f9fd08a58479c3de57483a17a0ec1ac3a
wonjinYi <studioplug17@gmail.com>
2021-01-12 23:59:04 +0900

    textfield caption color

--- front/src/components/InputSection.js
+++ front/src/components/InputSection.js
@@ -1,12 +1,38 @@
 import styled from "styled-components";
+
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles({
+    root : {
+        
+    },
+    textfield : {
+        borderColor : 'white',
+        '& label' : {
+            color : 'white',
+        },
+        '& label.Mui-focused': {
+            color: 'white',
+        },
+        '&.MuiTextField-root .MuiOutlinedInput-notchedOutline': {
+            color : 'white',
+            borderColor : 'white',
+        }
+    }
+});
+
+// const RunButton = withstyles(
+// })(Button);
+
 export default function InputSection() {
+    const classes = useStyles();
+
     return (
         <InputSectionWrap>
-            <TextField id="standard-basic" label="Text" />
-            <TextField id="standard-basic" label="Reps" />
+            <TextField id="text-input" className={classes.textfield} label="Text" />
+            <TextField id="reps-input" className={classes.textfield} label="Reps" />
             <Button variant="contained">DO REPEAT</Button>
         </InputSectionWrap>
     );
